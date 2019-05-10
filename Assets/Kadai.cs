193be3f414f38099f9cafa1195d3930f69c1b3e2
原　諒太 <ryopityu@yahoo.co.jp>
2019-05-10 22:56:11 +0900

    Lesson4　課題のコミット

---
+++ Assets/Kadai.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using UnityEngine;
+
+//Bossクラス（＝設計図）
+public class Boss
+{
+    private int hp = 100;       // 体力
+    private int power = 25;     // 攻撃力
+    private int mp = 53;        // 魔法力
+
+    //攻撃用の関数
+    //引数も返り値もない関数
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた！");
+    }
+
+    //防御用の関数
+    //引数があり、返り値がない関数
+    public void Defence(int damage)
+    {
+        Debug.Log(damage + "のダメージを受けた！");
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+
+    //魔法攻撃用の関数
+    public void Magic()
+    {
+        if (this.mp >= 5)
+        {
+            // 残りmpを減らす
+            this.mp -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+}
+
+//Kadaiクラス（＝本体・設計図を使うところ）
+public class Kadai : MonoBehaviour
+{
+    void Start()
+    {
+        //Bossクラスの変数(lastboss)を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        // 魔法攻撃用の関数を呼び出す
+        for (int i = 0; i < 11; i++)
+        {
+            lastboss.Magic();
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    // Use this for initialization
+    private void Start()
+    {
+        //配列を初期化する
+        int[] array = { 9, 6, 3, 1, 0 };
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        for (int i = 4; i >= 0; i--)
+        {
+                Debug.Log(array[i]);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
